e636209f152aad73c6b5174bedd6b87250495780
MARSICULUS <91570512+MARSICULUS@users.noreply.github.com>
2021-11-23 18:39:21 +0000

    esercitazione01/esercizio01 primo punto

--- esercitazione01/esercizio01.c
+++ esercitazione01/esercizio01.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int fattoriale(int k)
+{
+    if(k == 0)
+        return 1;
+    return k * fattoriale(k - 1);
+}
+
+int potenza(int a, int b)
+{
+    
+}
+
 int main()
 {
     printf("hello word\n");
