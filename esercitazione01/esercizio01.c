808284262be8b773bb851403d2d9b49838da9103
MARSICULUS <91570512+MARSICULUS@users.noreply.github.com>
2021-11-23 21:24:48 +0000

    esercitazione01/esercizio01.c secondo punto

--- esercitazione01/esercizio01.c
+++ esercitazione01/esercizio01.c
@@ -7,14 +7,39 @@ int fattoriale(int k)
     return k * fattoriale(k - 1);
 }
 
-int potenza(int a, int b)
+float potenza(float a, int b)
 {
-    
+    if(b == 0)
+        return 1;
+    return a * potenza(a, b - 1);
+}
+
+float sommatoria_iterativa(float x, int n)
+{
+    float somma = 0;
+    for(int i = 0; i <= n; i++)
+    {
+        somma = somma + ((float)potenza(x, i) / (float)fattoriale(i));
+    }
+    return somma;
+}
+
+float sommatoria_ricorsiva(float x, int n)
+{
+    if(n == 0)
+        return 1.0;
+    return (potenza(x, n) / (float)fattoriale(n)) + sommatoria_ricorsiva(x, n - 1);
 }
 
 int main()
 {
-    printf("hello word\n");
+    float x = 3.0;
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf("sommatoria_iterativa: %f\n", sommatoria_iterativa(x, i));
+        printf("sommatoria_ricorsiva: %f\n", sommatoria_ricorsiva(x, i));
+    }
 
     return 0;
 }
