a04aae752a928c4eebee1edc4af3ad5a703e04ca
MARSICULUS <91570512+MARSICULUS@users.noreply.github.com>
2021-11-24 14:42:31 +0000

    add funzione len iterativa

--- esercitazione01/esercizio03.c
+++ esercitazione01/esercizio03.c
@@ -7,12 +7,23 @@ int len(char str[], int i)
     return 1 + len(str, i + 1);
 }
 
+int len_iterativa(char str[], int i)
+{
+    int count = 0;
+    while(str[i] != '\0')
+        {
+            count++;
+            i++;
+        }
 
+    return count;
+}
 
 int main()
 {
     char stringa[7] = {'G', 'i', 'u', 'l', 'i', 'o'};
     printf("%d\n", len(stringa, 0));
+    printf("%d\n", len_iterativa(stringa, 0));
 
     return 0;
 }
