cc1b4b2ff622762988bb94cad3e0c50a41077d99
MARSICULUS <91570512+MARSICULUS@users.noreply.github.com>
2021-12-03 14:40:44 +0000

    bug ovunque

--- esercitazione02/esercizio02.c
+++ esercitazione02/esercizio02.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fibonazzi(int n)
+float fibonazzi(int n)
 {
-    int a = 1;
-    int b = 1;
-    int fibo;
+    float a = 1;
+    float b = 1;
+    float fibo;
 
     if(n == 0 || n == 1)
         return 1;
@@ -19,29 +19,45 @@ int fibonazzi(int n)
     return fibo;
 }
 
-int sequenza(int n)
+float sequenza(int n)
 {
     if(n ==0 || n == 1)
         return n;
     return (fibonazzi(n) + sequenza(n - 1)) / 2;
 }
 
+float sequenza_ite(int n)
+{
+    float n_0 = 1;
+
+    if(n == 0 || n == 1)
+        return n;
+    
+    for(int i = 2; i <= n; i++)
+    {
+        n_0 = (fibonazzi(n) + n_0) / 2;
+    }
+}
+
 int main()
 {
     for(int i = 0; i < 10; i++)
     {
-        printf("%d\n", fibonazzi(i));
+        printf("%f\n", fibonazzi(i));
     }
 
     printf("--------\n");
 
     for(int i = 0; i < 10; i++)
     {
-        printf("%d\n", sequenza(i));
+        printf("%d:\n", i);
+        printf("   %f\n", sequenza(i));
+        printf("   %f\n", sequenza_ite(i));
+
     }
 
     int dimensione = 10;
-    int* a = malloc(sizeof(int) * dimensione);
+    int* a = malloc(sizeof(float) * dimensione);
 
     for(int i = 0; i < dimensione; i++)
     {
