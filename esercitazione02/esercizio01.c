7de42d1587a62c9ccc7cac38cadde3fafa5e5a92
MARSICULUS <91570512+MARSICULUS@users.noreply.github.com>
2021-12-03 14:04:03 +0000

    ricorsiva

---
+++ esercitazione02/esercizio01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+//funzione per trovare un indice (super efficiente)
+int cantor(int arr[], int f, int i, int l)
+{
+    int m = (l + i)/2;
+
+    if(i > l)
+        return -1;
+    if(f == arr[m])
+        return m;
+    if(f > arr[m])
+        return cantor(arr, f, m + 1, l);
+    if(f < arr[m])
+        return cantor(arr, f, i, m - 1);
+}
+
+int main()
+{
+    int arrayi[7] = {3, 4, 6, 8, 21, 33, 55};
+    int x = 8;
+
+    printf("trovato nell'indice: %d\n", cantor(arrayi, x, 0, 9));
+
+
+    return 0;
+}
